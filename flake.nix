d5f8b81de3ec1ee51481bdb717bcd529a27c25f8
purepani <pani0028@umn.edu>
2023-03-22 18:07:45 +0000

    Fixed jupyter

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-     devenv.url = "github:cachix/devenv";
+    devenv.url = "github:cachix/devenv";
   };
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    devenv, 
+    devenv,
     ...
-  } @inputs:
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {};
 
@@ -22,16 +22,16 @@
         inherit inputs pkgs;
         modules = [
           ({pkgs, ...}: {
-            packages = [pkgs.zlib pkgs.python3 pkgs.nodejs pkgs.libgccjit];
+            packages = [pkgs.zlib pkgs.python3 pkgs.nodejs pkgs.libgccjit pkgs.xorg.libX11 pkgs.libGLU pkgs.libGL pkgs.ffmpeg pkgs.xorg.libXrender];
+            
             languages.python = {
               enable = true;
               venv.enable = true;
               poetry.enable = true;
             };
             languages.typescript = {
-              enable=true;
+              enable = true;
             };
-
           })
         ];
       };
