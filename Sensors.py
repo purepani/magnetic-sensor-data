e61a8abcabe57b59387285695915cd3630e591f0
purepani <pani0028@umn.edu>
2023-08-22 13:02:48 -0500

    changed to mT

--- Sensors.py
+++ Sensors.py
@@ -4,30 +4,46 @@ import smbus
 import adafruit_mlx90393
 import board
 
+
 class TLVSensor:
     def __init__(self):
         self.sensor = TLV.TLV493D()
-    
-    def get_magnetometer(self): #return tuple
-        self.sensor.update_data() 
+
+    def get_magnetometer(self):  # return tuple
+        self.sensor.update_data()
         x = self.sensor.get_x()
         y = self.sensor.get_y()
         z = self.sensor.get_z()
         return x, y, z
 
+
 class PIMSensor:
-    def __init__(self, address=0x1d, i2c_dev=1):
+    def __init__(self, address=0x1D, i2c_dev=1):
         self.sensor = lsm303d.LSM303D(address, i2c_dev=smbus.SMBus(i2c_dev))
-    
-    def get_magnetometer(self): #return tuple
+
+    def get_magnetometer(self):  # return tuple
         x, y, z = self.sensor.magnetometer()
-        return x/10, y/10, z/10
+        return x / 10, y / 10, z / 10
+
 
 class MLXSensor:
-    def __init__(self, address, gain=adafruit_mlx90393.GAIN_1X, resolution=adafruit_mlx90393.RESOLUTION_16, filt=adafruit_mlx90393.FILTER_7, oversampling=adafruit_mlx90393.OSR_3):
+    def __init__(
+        self,
+        address,
+        gain=adafruit_mlx90393.GAIN_1X,
+        resolution=adafruit_mlx90393.RESOLUTION_16,
+        filt=adafruit_mlx90393.FILTER_7,
+        oversampling=adafruit_mlx90393.OSR_3,
+    ):
         i2c = board.I2C()
-        self.sensor=adafruit_mlx90393.MLX90393(i2c, address=address, gain=gain, resolution=resolution, filt=filt, oversampling=oversampling)
-    
+        self.sensor = adafruit_mlx90393.MLX90393(
+            i2c,
+            address=address,
+            gain=gain,
+            resolution=resolution,
+            filt=filt,
+            oversampling=oversampling,
+        )
 
     def get_magnetometer(self):
-        x, y, z = self.sensor.magnetic
+        x, y, z = tuple(map(lambda x: x / 1000, self.sensor.magnetic))
